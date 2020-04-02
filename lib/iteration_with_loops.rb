88bc0db237eb6536333a11b0c141ae9522815aa5
Elias Taveras <eliastave12@gmail.com>
2020-04-02 01:23:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,8 +10,8 @@ def find_even_values(src)
       if src[row_index][element_index].even?
         src[row_index][element_index]
       element_index += 1
-      end
-      row_index += 1 
-    end
-  end
-end
+      end # if
+    end # inner while
+    row_index += 1
+  end # outter while
+end # def
